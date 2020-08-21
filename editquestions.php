d4c9d9e87a866b74d3f149a2891718d4c9735bbc
atul kumar <atul.11192@gmail.com>
2020-08-22 01:36:23 +0530

    adding more code to the project

--- editquestions.php
+++ editquestions.php
@@ -33,9 +33,6 @@
         <div class="form-group">
             <input type="text" name="option7" id="option7" class="form-control">
         </div>
-        <div class="form-group">
-            <button class="btn btn-primary">Update</button>
-        </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>

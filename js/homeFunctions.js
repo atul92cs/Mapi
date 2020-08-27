93e9c417c17bb6d773cdceea26c4bc4e58778cdd
atul kumar <atul.11192@gmail.com>
2020-08-27 21:40:33 +0530

    started adding charts

--- js/homeFunctions.js
+++ js/homeFunctions.js
@@ -7,7 +7,6 @@
  let dayyest=new Date().getDate()-2;
  let dateyest=yesterday+'-'+month2+'-'+year;
  let datebyest=dayyest+'-'+month2+'-'+year;
- console.log(datebyest);
 let rootRef=firebase.database().ref('/user');
 let yestcollection;
 let byestcollection;
@@ -20,4 +19,20 @@ rootRef.orderByChild('date').equalTo(datebyest).on('value',snapshot=>{
 });
 rootRef.orderByChild('date').equalTo(date).on('value',snapshot=>{
     todaycollection=snapshot.numChildren();
+});
+let myChart=document.getElementById('survey-form').getContext('2d');
+
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 18;
+    Chart.defaults.global.defaultFontColor = '#777';
+let surveyChart=new Chart(myChart,{
+  type:'bar',
+  data:{
+    labels:['today','yesterday','day before yesterday'],
+    data:[
+      todaycollection,
+      yestcollection,
+      byestcollection
+    ]
+  }
 });
--- js/userFunctions.js
+++ js/userFunctions.js
@@ -1,12 +1,3 @@
-let firebaseConfig = {
-    apiKey: "AIzaSyCRREradM-W9MET5-sRvZLxgHBCsGkQKnM",
-    authDomain: "mapi-1c5da.firebaseapp.com",
-    databaseURL: "https://mapi-1c5da.firebaseio.com",
-    projectId: "mapi-1c5da",
-    storageBucket: "mapi-1c5da.appspot.com",
-    messagingSenderId: "188474243832",
-    appId: "1:188474243832:web:2686d691478359bc9ed90d"
-  };
 
 const auth = firebase.auth();
 
@@ -70,4 +61,3 @@ signOut=()=>{
         window.location.replace('index.html');
     }, 1000);
 }
-console.log(db);

6116da9e4541784c147b30bccd7bc25d22425efb
atul kumar <atul.11192@gmail.com>
2020-08-27 19:11:46 +0530

    adding more date functionalities and other functionalities soon

--- js/homeFunctions.js
+++ js/homeFunctions.js
@@ -1,4 +1,8 @@
+ let day=new Date().getDate();
+ let month=new Date().getMonth();
+ let year=new Date().getFullYear();
+ let date=day+'-'+month+'-'+year;
 let rootRef=firebase.database().ref('/user');
-rootRef.orderByKey().on('value',snapshot=>{
+rootRef.orderByKey().equalTo(date).on('value',snapshot=>{
   console.log(snapshot.val());
 });

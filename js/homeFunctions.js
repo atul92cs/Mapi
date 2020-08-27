f1e905a5ff6e3967d3b1a7f05bb0a81b4bfe3961
atul kumar <atul.11192@gmail.com>
2020-08-27 21:05:10 +0530

    adding dates for redering chart

--- js/homeFunctions.js
+++ js/homeFunctions.js
@@ -2,7 +2,13 @@
  let month=new Date().getMonth();
  let year=new Date().getFullYear();
  let date=day+'-'+month+'-'+year;
+ let yesterday=new Date().getDate()-1;
+ let month2=new Date().getMonth()+1;
+ let dayyest=new Date().getDate()-2;
+ let dateyest=yesterday+'-'+month2+'-'+year;
+ let datebyest=dayyest+'-'+month2+'-'+year;
+ console.log(datebyest);
 let rootRef=firebase.database().ref('/user');
-rootRef.orderByKey().equalTo(date).on('value',snapshot=>{
+rootRef.orderByChild('date').equalTo(dateyest).on('value',snapshot=>{
   console.log(snapshot.val());
 });
